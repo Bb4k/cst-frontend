f2744d8941602f18c64f44a7ebc263e225c009d1
TIPYexe <robert-alexandru.nicolescu@my.fmi.unibuc.ro>
2023-04-02 02:02:38 +0300

    Progress page + applist component

--- CST-hack/components/index.js
+++ CST-hack/components/index.js
@@ -1,9 +1,13 @@
 import CustomInput from "./custom-input/custom-input.component";
 import CustomButton from "./custom-button/custom-button.components";
 import Footer from "./footer/footer.component";
+import AppList from "./app-list/app-list.component";
+import AppCategory from "./app-category/app-category.component";
 
 export{
     CustomInput,
     CustomButton,
-    Footer
+    Footer,
+    AppList,
+    AppCategory
 }
